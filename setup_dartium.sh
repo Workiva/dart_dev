78d38c59820c81a864736c2d44631eab7c0cd341
Christopher.Daly <christopher.daly@workiva.com>
2015-11-16 15:40:01 -0600

    Changed file permissions on bash script

--- setup_dartium.sh
+++ setup_dartium.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/bin/bash  
 
-set -e
+set -e  
 
 DART_DIST=dartsdk-linux-x64-release.zip
-DARTIUM_DIST=dartium-linux-x64-release.zip
+ DARTIUM_DIST=dartium-linux-x64-release.zip  
 
-echo Fetching dart sdk and Dartium
-curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST
+echo Fetching dart sdk and Dartium 
+
+curl http://storage.googleapis.com/dart-archive/channels/stable/release/latest/sdk/$DART_DIST > $DART_DIST 
 curl http://storage.googleapis.com/dart-archive/channels/stable/raw/latest/dartium/$DARTIUM_DIST > $DARTIUM_DIST
 
-unzip $DART_DIST > /dev/null
+  unzip $DART_DIST > /dev/null 
 unzip $DARTIUM_DIST > /dev/null
-rm $DART_DIST
-rm $DARTIUM_DIST
+
+ rm $DART_DIST
+ rm $DARTIUM_DIST  
 
 mv dartium-* dartium;
 
-ln -s "$PWD/dartium/chrome" /usr/local/bin/dartium
+  ln -s "$PWD/dartium/chrome" /usr/local/bin/dartium
