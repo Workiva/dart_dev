2cc8a05ff245d1c372a8aac87e1a93ffde3f948b
George Lesica <george.lesica@workiva.com>
2016-05-04 16:46:18 -0600

    Update completions and add comment.

--- tool/generate_completions.sh
+++ tool/generate_completions.sh
@@ -2,4 +2,9 @@
 
 set -e
 
-pub run completion:shell_completion_generator ddev > lib/bash-completion.sh
+# We support completions for two forms of the command. First
+# is `dart_dev`, which is the global version. Second is `ddev`
+# which, if configured according to the README, will only work
+# within a Dart project directory.
+
+pub run completion:shell_completion_generator dart_dev ddev> lib/bash-completion.sh
