07163358fdb583169778f3be97240283cf6e19ed
Jay.Udey <jay.udey@webfilings.com>
2016-10-20 11:44:17 -0500

    modifying tests to verify existing functionality

--- test_fixtures/gen_test_runner/check_pass/tool/dev.dart
+++ test_fixtures/gen_test_runner/check_pass/tool/dev.dart
@@ -5,5 +5,9 @@ import 'package:dart_dev/src/tasks/gen_test_runner/config.dart';
 
 main(List<String> args) async {
 
+  config.genTestRunner.configs = [
+    new TestRunnerConfig(directory: 'test/', env: Environment.browser, genHtml: true)
+  ];
+
   await dev(args);
 }
