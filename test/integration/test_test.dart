820ef48f08ed90eac2e5f43529962dba5a45673a
Pat Kujawa <pat.kujawa@webfilings.com>
2018-03-08 15:27:47 -0700

    Add a test for test task and stderr; skip, though - it's weird.

--- test/integration/test_test.dart
+++ test/integration/test_test.dart
@@ -32,14 +32,24 @@ const String projectWithPassingIntegrationTests =
     'test_fixtures/test/passingIntegration';
 const String projectThatNeedsPubServe = 'test_fixtures/test/needs_pub_serve';
 
-Future<int> runTests(String projectPath,
-    {bool unit: true,
-    bool integration: false,
-    List<String> files,
-    String testName: '',
-    bool runCustomPubServe: false,
-    int pubServePort: 56090,
-    String webCompiler}) async {
+class TaskResponse {
+  bool successfulExit;
+  List<String> outputs = [];
+  List<String> errors = [];
+}
+
+Future<int> runTests(
+  String projectPath, {
+  bool unit: true,
+  bool integration: false,
+  List<String> files,
+  String testName: '',
+  bool runCustomPubServe: false,
+  int pubServePort: 56090,
+  String webCompiler,
+  TaskResponse response,
+}) async {
+  response ??= new TaskResponse();
   await Process.run('pub', ['get'], workingDirectory: projectPath);
 
   List args = ['run', 'dart_dev', 'test', '--no-color'];
@@ -89,6 +99,13 @@ Future<int> runTests(String projectPath,
   TaskProcess process =
       new TaskProcess('pub', args, workingDirectory: projectPath);
 
+  process.stdout.listen(print);
+  process.stderr.listen(print);
+  StreamSubscription<String> outSub =
+      process.stdout.listen(response.outputs.add);
+  StreamSubscription<String> errSub =
+      process.stderr.listen(response.errors.add);
+
   var numTestsRun = -1;
   var webCompilerSpecified = false;
 
@@ -102,9 +119,18 @@ Future<int> runTests(String projectPath,
     }
   });
 
-  await process.done;
-  if ((await process.exitCode) != 0)
-    throw new TestFailure('Expected test to pass.');
+  try {
+    await process.done;
+    if ((await process.exitCode) != 0)
+      throw new TestFailure('Expected test to pass.');
+  } finally {
+    try {
+      await outSub.cancel();
+      await errSub.cancel();
+    } catch (e) {
+      print(e);
+    }
+  }
 
   if (pubServeProcess != null) {
     pubServeProcess.kill();
@@ -125,6 +151,33 @@ void main() {
       expect(runTests(projectWithFailingTests, unit: true, integration: false),
           throwsA(new isInstanceOf<TestFailure>()));
     });
+
+    test('should include test failure in stderr if some unit tests fail',
+        () async {
+      var response = new TaskResponse();
+//      runTests(projectWithPassingTests, unit: true, integration: false, response: response);
+      expect(
+          runTests(projectWithFailingTests,
+              unit: true, integration: false, response: response),
+          throwsA(new isInstanceOf<TestFailure>()));
+
+      print(response.outputs.join('\n'));
+      print('out len=${response.outputs.length}');
+      print(response.errors.join('\n'));
+      print('err len=${response.errors.length}');
+      expect(response.errors, isNotEmpty);
+
+      expect(
+          response.errors.join(),
+          stringContainsInOrder([
+            '00:00 +0 -1: fails [E]',
+            'Expected: true',
+            'Actual: <1>',
+            'package:test/src/frontend/expect.dart',
+          ]));
+    },
+        skip:
+            'This test is not getting any output at all and I do not know why');
 
     test('should fail if some integration tests fail', () async {
       expect(runTests(projectWithFailingTests, unit: false, integration: true),
