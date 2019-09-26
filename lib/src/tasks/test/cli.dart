f20908ba281b05666b27a935cb758112431c8f70
Corwin Sheahan <corwin.sheahan@workiva.com>
2019-09-26 13:50:40 -0600

    write runners with their original content

--- lib/src/tasks/test/cli.dart
+++ lib/src/tasks/test/cli.dart
@@ -172,38 +172,41 @@ class TestCli extends TaskCli {
           'files/directories');
     }
 
+    final mapRunnerToContents = <File /*generated runner file*/, String /* contents */>{};
     // Build the list of tests to run.
     if (individualTestsSpecified) {
       // Individual tests explicitly passed in should override the test suites.
       if (dartMajorVersion == 2 && parsedArgs[_hackFastBuilds]) {
         reporter.warning(
-            'You\'re using `${_hackFastBuilds}`. This will re-write the generated test runners in your repo.\n'
+            'WARNING: You\'re using `${_hackFastBuilds}`. This will re-write the generated test runners in your repo.\n'
                 'The test task will attempt to restore your generated runners after completion, but you may '
-                'have to re-run `pub run dart_dev gen-test-runner` if your runners have changed.');
+                'have to re-run `pub run dart_dev gen-test-runner` and `pub run dart_dev format` if your runners have changed.\n\n');
         final mapConfigToTestFiles = <TestRunnerConfig,
-            List<String> /* tests to include in runner */>{};
+            Set<String> /* tests to include in runner */>{};
         // Construct mapping from config to tests which should be ran in that config
-        for (final _config in config.genTestRunner.configs) {
+        final copyOfConfigs = new List.from(config.genTestRunner.configs);
+        for (final _config in copyOfConfigs) {
           for (final testFilePath in parsedArgs.rest) {
             if (testFilePath.contains(_config.directory)) {
-              mapConfigToTestFiles.containsKey(_config)
-                  ? mapConfigToTestFiles[_config].add(testFilePath)
-                  : mapConfigToTestFiles[_config] = [testFilePath];
+              mapConfigToTestFiles.putIfAbsent(_config, () => new Set.from([testFilePath]));
+              mapConfigToTestFiles[_config].add(testFilePath);
             }
           }
         }
 
         for (final _config in mapConfigToTestFiles.keys) {
-          config.genTestRunner.configs.remove(_config);
+          copyOfConfigs.remove(_config);
+          final runnerFile = new File(_config.path);
+          mapRunnerToContents.putIfAbsent(runnerFile, () => runnerFile.readAsStringSync());
           await genTestRunner(_config,
-              filesToInclude: mapConfigToTestFiles[_config]);
+              filesToInclude: mapConfigToTestFiles[_config].toList());
           tests.add(_config.path);
         }
 
         // Empty all other unused generated runners
-        for (final _config in config.genTestRunner.configs) {
+        for (final _config in copyOfConfigs) {
           await genTestRunner(_config, filesToInclude: []);
-          }
+        }
       } else {
         tests.addAll(parsedArgs.rest);
       }
@@ -329,9 +332,9 @@ A pub serve instance will not be started.''');
 
     if (parsedArgs[_hackFastBuilds]) {
       // Regenerate all runners:
-      for (final _config in config.genTestRunner.configs) {
-        await genTestRunner(_config);
-      }
+      mapRunnerToContents.forEach((file, originalContents) {
+        file.writeAsStringSync(originalContents);
+      });
     }
 
     return task.successful
