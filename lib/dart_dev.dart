86007e171e2464c7ea88e27d55bc6ae6490fe723
evanweible-wf <evan.weible@workiva.com>
2019-10-15 00:29:14 -0600

    Break compose() up into chainTool() and setUpTool()

--- lib/dart_dev.dart
+++ lib/dart_dev.dart
@@ -1,6 +1,7 @@
 export 'src/core_config.dart' show coreConfig;
 export 'src/dart_dev_tool.dart'
     show DevTool, DevToolCommand, DevToolExecutionContext;
+export 'src/tools/aggregate_tool.dart' show chainTool, setUpTool;
 export 'src/tools/analyze_tool.dart' show AnalyzeTool;
 export 'src/tools/dart_function_tool.dart'
     show DartFunctionTool, DartFunctionToolContext;
--- lib/src/utils/compose.dart
+++
@@ -1,8 +0,0 @@
-import '../dart_dev_tool.dart';
-import '../tools/aggregate_tool.dart';
-
-AggregateTool compose(DevTool tool,
-        {List<DevTool> before, List<DevTool> after}) =>
-    AggregateTool(tool)
-      ..before = before
-      ..after = after;
