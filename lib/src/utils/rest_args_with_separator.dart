24d8b5d11bf67156211fdce0de14df71ff9b4fea
evanweible-wf <evan.weible@workiva.com>
2021-12-15 09:25:36 -0700

    Better variable naming

--- lib/src/utils/rest_args_with_separator.dart
+++ lib/src/utils/rest_args_with_separator.dart
@@ -25,20 +25,20 @@ List<String> restArgsWithSeparator(ArgResults argResults) {
 
   final args = argResults.arguments;
   final rest = argResults.rest;
-  var rCursor = 0;
-  for (var aCursor = 0; aCursor < args.length; aCursor++) {
+  var restIndex = 0;
+  for (var argsIndex = 0; argsIndex < args.length; argsIndex++) {
     // Iterate through the original args until we hit the first separator.
-    if (args[aCursor] == '--') break;
+    if (args[argsIndex] == '--') break;
     // While doing so, move a cursor through the rest args list each time we
     // match up between the original list and the rest args list. This works
     // because the rest args list should be an ordered subset of the original
     // args list.
-    if (args[aCursor] == rest[rCursor]) {
-      rCursor++;
+    if (args[argsIndex] == rest[restIndex]) {
+      restIndex++;
     }
   }
 
-  // At this point, [rCursor] should be pointing to the spot where the first arg
-  // separator should be restored.
-  return [...rest.sublist(0, rCursor), '--', ...rest.sublist(rCursor)];
+  // At this point, [restIndex] should be pointing to the spot where the first
+  // arg separator should be restored.
+  return [...rest.sublist(0, restIndex), '--', ...rest.sublist(restIndex)];
 }
