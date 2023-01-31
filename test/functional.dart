f71d6697893f9d3b43104735f212db30005e89e8
evanweible-wf <evan.weible@workiva.com>
2023-01-30 20:05:23 -0700

    Use POSIX style paths in functional tests pubspecs

--- test/functional.dart
+++ test/functional.dart
@@ -34,13 +34,14 @@ Future<TestProcess> runDevToolFunctionalTest(
       .where((f) => p.basename(f.path) == 'pubspec.yaml')
       .map((f) => f.absolute);
   final pathDepPattern = RegExp(r'path: (.*)');
+  final posix = p.Context(style: p.Style.posix);
   for (final pubspec in pubspecs) {
     final updated =
         pubspec.readAsStringSync().replaceAllMapped(pathDepPattern, (match) {
       final relDepPath = match.group(1);
-      final relPubspecPath = p.relative(pubspec.path, from: d.sandbox);
-      final absPath = p.absolute(p.normalize(
-          p.join(templateDir.path, relPubspecPath, relDepPath, '..')));
+      final relPubspecPath = posix.relative(pubspec.path, from: d.sandbox);
+      final absPath = posix.absolute(posix.normalize(
+          posix.join(templateDir.path, relPubspecPath, relDepPath, '..')));
       return 'path: $absPath';
     });
     pubspec.writeAsStringSync(updated);
