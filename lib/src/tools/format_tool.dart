3432a93ea395bf340d1d778e5f80b54a77db8786
evanweible-wf <evan.weible@workiva.com>
2023-02-13 20:21:55 -0700

    Fix unnecessary import

--- lib/src/tools/format_tool.dart
+++ lib/src/tools/format_tool.dart
@@ -9,7 +9,6 @@ import 'package:io/io.dart' show ExitCode;
 import 'package:logging/logging.dart';
 import 'package:path/path.dart' as p;
 
-import '../../utils.dart';
 import '../dart_dev_tool.dart';
 import '../utils/arg_results_utils.dart';
 import '../utils/assert_no_positional_args_nor_args_after_separator.dart';
